072ad44c34adb7e2b4e1348a8524a2dcd2f9913d
Kalana_KD <kalanakd471@gmail.com>
2025-04-25 23:12:13 +0530

    using useEffect

--- useEffect.jsx
+++ useEffect.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
 function NewFollowerAlert() {
+  // This component will alert the user when they get a new follower
+  // It uses the useEffect hook to run a side effect when the followers state changes
   const [followers, setFollowers] = useState(0);
 
   useEffect(() => {
